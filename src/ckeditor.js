2383a3bb101aec65078852ae877f0f210151c621
Igal Kleiner <Igal-Kleiner@users.noreply.github.com>
2020-04-10 20:25:02 +0300

    Restored an import

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,7 +11,7 @@ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
-// import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
+import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
@@ -67,7 +67,7 @@ AdvancedEditor.builtinPlugins = [
 	Italic,
 	BlockQuote,
 	CKFinder,
-	// CKFinderUploadAdapter,
+	CKFinderUploadAdapter,
 	EasyImage,
 	FontBackgroundColor,
 	FontColor,
